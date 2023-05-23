1de9cf3eb9a287997a5a09483c22843635ee39f0
ihrow <ihrow@proton.me>
2023-05-23 13:26:31 +0200

    window fix + canvas

--- src/main/java/sk/stuba/fei/uim/oop/gui/Canvas.java
+++ src/main/java/sk/stuba/fei/uim/oop/gui/Canvas.java
@@ -1,4 +1,65 @@
 package sk.stuba.fei.uim.oop.gui;
 
-public class Canvas {
+import lombok.Data;
+import lombok.Getter;
+import sk.stuba.fei.uim.oop.shapes.Circle;
+import sk.stuba.fei.uim.oop.shapes.Rectangle;
+import sk.stuba.fei.uim.oop.shapes.Shape;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+@Data
+public class Canvas extends JPanel {
+
+    private ArrayList<Shape> shapes;
+    private int length;
+    private int spacing;
+
+    public Canvas() {
+        this.shapes = new ArrayList<>();
+        this.length = 70;
+        this.spacing = 5;
+    }
+
+    public void removeShapes() {
+        if (this.shapes.size() > this.length) {
+            this.shapes.remove(0);
+        }
+    }
+
+    public void changeShapes(String shape) {
+        ArrayList<Shape> newShapeList = new ArrayList<>();
+
+        for (Shape s : this.shapes) {
+            if (shape.equals("CIRCLE")) {
+                newShapeList.add(new Circle(s.getX(), s.getY(), s.getRadius()));
+            } else if (shape.equals("SQUARE")) {
+                newShapeList.add(new Rectangle(s.getX(), s.getY(), s.getRadius()));
+            }
+         }
+
+        this.shapes = newShapeList;
+    }
+
+    public void changeRadius(int radius) {
+        for (Shape s : this.shapes) {
+            s.setRadius(radius);
+        }
+    }
+
+
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponents(g);
+        this.removeShapes();
+        for (int i = 0; i < this.shapes.size(); i++) {
+            if (i == 0 || i == this.shapes.size() - 1 || i % this.spacing == 0) {
+                this.shapes.get(i).paintComponent(g);
+            }
+        }
+    }
 }
+
--- src/main/java/sk/stuba/fei/uim/oop/gui/Window.java
+++ src/main/java/sk/stuba/fei/uim/oop/gui/Window.java
@@ -1,18 +1,70 @@
 package sk.stuba.fei.uim.oop.gui;
 
+import sk.stuba.fei.uim.oop.controls.GameLogic;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class Window {
+
+
     public Window() {
         JFrame frame = new JFrame("ExamRT");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 800);
-        frame.getContentPane().setBackground(new Color(0x333333));
+        frame.getContentPane().setBackground(new Color(0xC2BFBF));
         frame.setResizable(false);
         frame.setFocusable(true);
 
+        GameLogic logic = new GameLogic(frame);
 
+        JPanel menu = new JPanel(new BorderLayout());
+        JPanel labelsPanel = new JPanel(new GridLayout(1, 3));
+
+        JLabel lengthLabel = new JLabel("Length");
+        labelsPanel.add(lengthLabel);
+        JLabel radiusLabel = new JLabel("Radius");
+        labelsPanel.add(radiusLabel);
+        JLabel spacingLabel = new JLabel("Spacing");
+        labelsPanel.add(spacingLabel);
+        menu.add(labelsPanel, BorderLayout.PAGE_START);
+
+        JPanel sliders = new JPanel(new GridLayout(1, 3));
+        JSlider length = new JSlider(JSlider.VERTICAL, 20, 200, 50);
+        length.setMajorTickSpacing(10);
+        length.setSnapToTicks(true);
+        length.setPaintTicks(true);
+        length.setPaintLabels(true);
+        length.addChangeListener(logic);
+        sliders.add(length);
+
+
+        JSlider radius = new JSlider(JSlider.VERTICAL, 1, 20, 5);
+        radius.setMajorTickSpacing(1);
+        radius.setSnapToTicks(true);
+        radius.setPaintTicks(true);
+        radius.setPaintLabels(true);
+        radius.addChangeListener(logic);
+        sliders.add(radius);
+
+
+        JSlider spacing = new JSlider(JSlider.VERTICAL, 1, 20, 5);
+        spacing.setMajorTickSpacing(2);
+        spacing.setSnapToTicks(true);
+        spacing.setPaintTicks(true);
+        spacing.setPaintLabels(true);
+        spacing.addChangeListener(logic);
+        sliders.add(spacing);
+
+
+        String shapes[] = {"CIRCLE", "SQUARE", "HOURGLASS"};
+        JComboBox box = new JComboBox(shapes);
+        box.addActionListener(logic);
+
+        menu.add(sliders);
+        menu.add(box, BorderLayout.PAGE_END);
+
+        frame.add(menu, BorderLayout.EAST);
 
         frame.setVisible(true);
     }
